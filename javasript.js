d7296cc38ac0d7f65bc223015c60896357dc6a71
Nurbolat <nurbilim.kz@gmail.com>
2022-12-20 14:13:12 +0600

    Changes the title in 'index.html' file from 'Document' to 'Rock-Paper-Scissors'.
    Creates a function called
    'playRound(playerSelection, computerSelection)' that
    returns a string that declares the winner of the round.

--- javasript.js
+++ javasript.js
@@ -6,8 +6,30 @@ function getComputerChoice() {
     } else if (choice == 0) {
         return "Paper";
     } else {
-        return "Scissors";
+        return "Scissor";
     }
 }
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissor") {
+        return "I Lose!:( Rock beats Scissor";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissor") {
+        return "You Lose! Scissor beats Paper";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
+        return "I Lose!:( Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === "scissor" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissor";
+    } else if (playerSelection.toLowerCase() === "scissor" && computerSelection === "Paper") {
+        return "I Lose!:( Scissor beats Paper";
+    } else {
+        return "It's tie!";
+    }
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
